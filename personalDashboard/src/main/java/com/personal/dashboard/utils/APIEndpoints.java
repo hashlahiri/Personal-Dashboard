5edc738c4874ea35e24a237d0e22c10330d09e1f
C Lahiri <helloclahiri@outlook.com>
2024-06-08 20:12:46 +0530

    Minor changes - remove the extra paths from the url

--- personalDashboard/src/main/java/com/personal/dashboard/utils/APIEndpoints.java
+++ personalDashboard/src/main/java/com/personal/dashboard/utils/APIEndpoints.java
@@ -7,10 +7,8 @@ package com.personal.dashboard.utils;
 public final class APIEndpoints {
 
 	public static final String BASE_API_URL = "/api";
-	public static final String BASE_VERSION = "/v1";
-	public static final String BASE_API_URL_V1 = BASE_API_URL + BASE_VERSION;
-	public static final String USERINFO_API_URL = BASE_API_URL_V1 + "/userInfo";
-	public static final String CURRENT_WEATHER_API_URL = BASE_API_URL_V1 + "/currentWeather";
-	public static final String JSON_COMPARE_API_URL = BASE_API_URL_V1 + "/json";
+
+	public static final String CURRENT_WEATHER_API_URL = BASE_API_URL + "/currentWeather";
+	public static final String JSON_COMPARE_API_URL = BASE_API_URL + "/json";
 
 }
